151cbd15e93055a523e9ea80de18274d78ee1df8
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-04-30 00:22:52 +0500

    created AppDbContext

---
+++ src/FTech.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FTech.Domain.Entities.Auth;
+using Microsoft.EntityFrameworkCore;
+
+namespace FTech.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+           Database.Migrate();
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
