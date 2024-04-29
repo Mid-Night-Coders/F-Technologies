3d2c803e990e10063967c8e8113d02f9f6191dc5
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-04-30 00:22:23 +0500

    created enum

---
+++ src/FTech.Domain/Enums/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTech.Domain.Enums
+{
+    public enum Role
+    {
+        Admin = 1, Client, Employee
+    }
+}
