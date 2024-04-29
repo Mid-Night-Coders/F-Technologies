cc792bce3b3fe5d3dba7145ea1844a8e01a6cee6
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-04-30 00:22:08 +0500

    created auth entities

---
+++ src/FTech.Domain/Entities/Auth/User.cs
@@ -0,0 +1,14 @@
+﻿using FTech.Domain.Enums;
+
+namespace FTech.Domain.Entities.Auth
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string PhoneNumber { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpireDate { get; set; }
+
+        public Role Roles { get; set; }
+    }
+}
