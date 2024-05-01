1c29bd1ff3c3bbfcfdba95b7d7490f4175d877a7
Sardor <sardorstudent0618@gmail.com>
2024-05-02 00:57:18 +0500

    Controller createf

--- src/FTech.Presentation/Program.cs
+++ src/FTech.Presentation/Program.cs
@@ -1,11 +1,14 @@
+using FTech.Application;
+using FTech.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
